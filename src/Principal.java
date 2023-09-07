0be6399cdd91b0aaac8e4d395c11bc53cf9db990
Kelvem Loubach <kelvemloubach@outlook.com>
2023-09-07 20:06:00 -0300

    :sparkles: feat: Método para sacar dinheiro

--- src/Principal.java
+++ src/Principal.java
@@ -9,50 +9,71 @@ public class Principal {
 
         ContaTerminal contaTerminal = new ContaTerminal();
 
-        boolean controlLoop = false;
+        try {
 
-        while(!controlLoop){
+            System.out.println("Por favor, digite o número da agência;");
+            String agency = scanner.nextLine();
+            contaTerminal.setAgency(agency);
 
-        try{
+            System.out.println("Por favor, digite seu nome completo;");
+            String nameCliente = scanner.nextLine();
+            contaTerminal.setClientName(nameCliente);
 
-        System.out.println("Por favor, digite o número da agência;");
-        String agency = scanner.nextLine();
-        contaTerminal.setAgency(agency);
+            System.out.println("Por favor, digite o número da conta;");
+            int numberAccount = scanner.nextInt();
+            contaTerminal.setNumber(numberAccount);
 
-        System.out.println("Por favor, digite seu nome completo;");
-        String name = scanner.nextLine();
-        contaTerminal.setClientName(name);
+            System.out.println("Por favor, digite o valor para déposito;");
+            double balanceAccoutn = scanner.nextDouble();
+            contaTerminal.setBalance(balanceAccoutn);
 
-        System.out.println("Por favor, digite o número da conta;");
-        int numberAccount = scanner.nextInt();
-        contaTerminal.setNumber(numberAccount);
+            String getName = contaTerminal.getClientName();
+            String getAgency = contaTerminal.getAgency();
+            double getBalance = contaTerminal.getBalance();
+            int getNumber = contaTerminal.getNumber();
 
-        System.out.println("Por favor, digite o valor para déposito;");
-        double balance = scanner.nextDouble();
-        contaTerminal.setBalance(balance);
+            String message = """
 
-        scanner.close();
+                    ____________________________________________________________________________________________________________________
 
-        String getName = contaTerminal.getClientName();
-        String getAgency = contaTerminal.getAgency();
-        double getBalance = contaTerminal.getBalance();
-        int getNumber = contaTerminal.getNumber();
+                            Olá %s, obrigado por criar uma conta em nosso banco!
+                            Sua agência é: %s,
+                            Sua conta é: %d
+                            Seu saldo %.2f já está disponível para saque!
+                    _____________________________________________________________________________________________________________________
+                            """
+                    .formatted(getName, getAgency, getNumber, getBalance);
 
-       String message = """
-                        Olá %s, obrigado por criar uma conta em nosso banco!
-                        sua agência é %s, 
-                        conta %d
-                        seu saldo %.2f já está disponível para saque!               
-                        """.formatted(getName,getAgency,getNumber, getBalance );
+            System.out.println(message);
 
-        System.out.println(message);
-        controlLoop = true; 
+            System.out.println("Deseja sacar algum valor? ( Digite 0 para não e 1 para sim)");
+            int drawOrNot = scanner.nextInt();
 
-        }catch(java.util.InputMismatchException erro){
-            System.out.println("Algum dado foi digitado incorretamente!");
+            switch (drawOrNot) {
+                case 1:
+                    System.out.println("Qual o valor você quer sacar?");
+                    double withdrawal = scanner.nextDouble();
+                    double resultBalance = contaTerminal.withdrawMoney(withdrawal);
+
+                    if (resultBalance == -1) {
+                        System.out.println("Saldo insuficiente para esta operação!");
+                    }
+
+                    System.out.println("Seu saldo atual é: " + resultBalance);
+
+                    break;
+
+                case 0:
+                    break;
+
+            }
+
+            scanner.close();
+
+        } catch (java.util.InputMismatchException erro) {
+            System.out.println("Algum dado foi inserido incorretamente!");
         }
+
     }
+
 }
-
- }
-
